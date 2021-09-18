308cb54b80b03d97c6a46a614a9e6866127c7110
pavangole5 <golepavan48@gmail.com>
2021-09-18 19:27:35 +0530

    Update and now Beautifutl

---
+++ Grid/grid.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {addElement()};
+let i = 0;
+function addElement () {
+  // create a new div element
+  let main = document.getElementById("container")
+
+  // and give it some content
+  const newContent = document.createElement("div")
+  newContent.classList.add("box")
+  const image = document.createElement("img");
+  image.src="https://source.unsplash.com/random/" + i
+  i = i + 1;
+  newContent.append(image)
+  // add the text node to the newly created div
+  main.appendChild(newContent)
+}
